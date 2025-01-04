dcccee06ab85a3453a0d546126c8b92ae38de771
mohd Rizwan <rizuart23@gmail.com>
2025-01-04 22:32:50 +0530

    end of day 2

---
+++ Day-2/Exercise/Finding.py
@@ -0,0 +1,9 @@
+#8. First and Last Colors
+
+#Write a Python program to display the first and last colors from the following list.
+#color_list = ["Red","Green","White" ,"Black"]
+#Click me to see the sample solution
+
+color_list = ["Red", "Green", "White", "Black"]
+
+print("%s %s" % (color_list[0], color_list[-1]))
